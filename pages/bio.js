e3eaf2ef799dd70d13b44c514f87a7b6707727bf
Alex Sanders <a@snd.rs>
2019-02-20 18:35:18 +0000

    whooops

--- pages/bio.js
+++ pages/bio.js
@@ -1,28 +1,26 @@
 import Head from 'next/head';
 import Bio from '../content/bio.md';
 
-import { resolve } from 'styled-jsx/css';
-
 export default () => (
-	<div id="page">
-		<Head>
-			<title>Dan Tombs’ Bio</title>
-		</Head>
-		<Bio />
-		<style jsx global>{`
-			#page {
-				max-width: 60ch;
-				margin: 2em auto;
-			}
-			p,
-			h1,
-			h2,
-			h3,
-			h4,
-			h5,
-			li {
-				margin-bottom: 1rem;
-			}
-		`}</style>
-	</div>
+    <div id="page">
+        <Head>
+            <title>Dan Tombs’ Bio</title>
+        </Head>
+        <Bio />
+        <style jsx global>{`
+            #page {
+                max-width: 60ch;
+                margin: 2em auto;
+            }
+            p,
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            li {
+                margin-bottom: 1rem;
+            }
+        `}</style>
+    </div>
 );
