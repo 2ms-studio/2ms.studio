8da6cb7dc587fc87e59ac1ad8dbdbf624fc9beac
Alex Sanders <a@snd.rs>
2019-02-21 00:20:10 +0000

    🙏

--- api/readable.js
+++ api/readable.js
@@ -1,37 +1,18 @@
 const { parse } = require('url');
-const read = require('node-readability');
+const { promisify } = require('util');
+const read = promisify(require('node-readability'));
 
-module.exports = (req, res) => {
-    console.log(1);
-
+module.exports = async (req, res) => {
     try {
         const {
             query: { url },
         } = parse(req.url, true);
-        console.log(2);
 
         if (!url) throw 'no URL param';
 
-        console.log(3);
-
-        read(url, function(err, { content, close }, meta) {
-            console.log(4);
-            try {
-                close();
-                if (err) throw err;
-                if (meta.statusCode !== 200)
-                    throw `Error: ${meta.statusCode}\n${url} does not exist`;
-                console.log(5);
-                res.end(content);
-                console.log('5.1');
-            } catch (e) {
-                console.log(6);
-                res.statusCode = 500;
-                res.end(e);
-            }
-        });
+        const { content, close } = await read(url);
+        res.end(content);
     } catch (e) {
-        console.log(7);
         res.statusCode = 500;
         res.end(e);
     }
