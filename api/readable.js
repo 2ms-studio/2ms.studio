e02f1ecf58a5ac31261106852704308743365af0
Alex Sanders <a@snd.rs>
2019-02-21 15:01:30 +0000

    use ascarpe

--- api/readable.js
+++ api/readable.js
@@ -1,31 +1,27 @@
 const { parse } = require('url');
-const { promisify } = require('util');
-const read = require('node-readability');
+
+const scrape = require('ascrape');
 
 module.exports = (req, res) => {
     try {
         const {
             query: { url },
         } = parse(req.url, true);
-
         if (!url) throw 'no URL param';
 
-        console.log(typeof url);
-
-        read(url, function(err, article, meta) {
+        scrape(url, (err, { content }, meta) => {
             if (err) {
                 res.statusCode = 500;
                 res.end(err.message);
             }
 
-            if (meta.statusCode !== 200) {
-                res.statusCode = 500;
-                res.end(`Readability error: ${meta.statusCode}`);
+            if (content) {
+                res.statusCode = 200;
+                res.end(content.html());
             }
 
-            res.statusCode = 200;
-            res.end(article.content);
-            article.close();
+            res.statusCode = 500;
+            res.end('no article found');
         });
     } catch (e) {
         res.statusCode = 500;
