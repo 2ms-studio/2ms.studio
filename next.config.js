8409cab50c766f6de39b72030e18a3df83a953a6
Alex Sanders <a@snd.rs>
2019-02-22 15:51:13 +0000

    Revert "this?"
    This reverts commit 442491c7d6a3e8b07e5d235ef5adebaa94a07c2e.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
-const { PHASE_PRODUCTION_SERVER } =
-    process.env.NODE_ENV === 'development'
-        ? {}
-        : require('next-server/constants');
+const withMDX = require('@zeit/next-mdx')({
+    extension: /\.(md|mdx)$/,
+});
 
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === PHASE_PRODUCTION_SERVER) {
-        return {
-            /* production only config */
-        };
-    }
-    const withMDX = require('@zeit/next-mdx')({
-        extension: /\.(md|mdx)$/,
-    });
+const withPreact = require('@zeit/next-preact');
 
-    const withPreact = require('@zeit/next-preact');
-    return withPreact(
-        withMDX({
-            pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-            target: 'serverless',
-        }),
-    );
-};
+module.exports = withPreact(
+    withMDX({
+        pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+        target: 'serverless',
+    }),
+);
