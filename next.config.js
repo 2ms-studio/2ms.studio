442491c7d6a3e8b07e5d235ef5adebaa94a07c2e
Alex Sanders <a@snd.rs>
2019-02-22 15:44:49 +0000

    this?

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
-const withMDX = require('@zeit/next-mdx')({
-    extension: /\.(md|mdx)$/,
-});
+const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === 'development'
+        ? {}
+        : require('next-server/constants');
 
-const withPreact = require('@zeit/next-preact');
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        return {
+            /* production only config */
+        };
+    }
+    const withMDX = require('@zeit/next-mdx')({
+        extension: /\.(md|mdx)$/,
+    });
 
-module.exports = withPreact(
-    withMDX({
-        pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-        target: 'serverless',
-    }),
-);
+    const withPreact = require('@zeit/next-preact');
+    return withPreact(
+        withMDX({
+            pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+            target: 'serverless',
+        }),
+    );
+};
