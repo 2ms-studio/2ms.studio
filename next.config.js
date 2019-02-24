2aeae22e2e40b9a567945e8312c6daeceaddc3f7
Alex Sanders <a@snd.rs>
2019-02-24 18:09:08 +0000

    hello?

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
+const withPlugins = require('next-compose-plugins');
+const withPreact = require('@zeit/next-preact');
+const withTypescript = require('@zeit/next-typescript');
 const withMDX = require('@zeit/next-mdx')({
     extension: /\.(md|mdx)$/,
 });
 
-const withPreact = require('@zeit/next-preact');
-
-module.exports = withPreact(
-    withMDX({
+module.exports = withPlugins(
+    ['withTypescript', 'withTypescript', 'withPreact', 'withMDX'],
+    {
         pageExtensions: ['js', 'jsx', 'md', 'mdx'],
         target: 'serverless',
-    }),
+    },
 );
