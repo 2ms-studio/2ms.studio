d618f33c5891d4852f77ed84dbd3f56556686bad
Alex Sanders <a@snd.rs>
2019-02-24 19:13:30 +0000

    re-add preact

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withTypescript = require('@zeit/next-typescript');
+const withPreact = require('@zeit/next-preact');
 const withMDX = require('@zeit/next-mdx')({
     extension: /\.(md|mdx)$/,
 });
 
-module.exports = withMDX(
-    withTypescript({
-        pageExtensions: ['tsx', 'md', 'mdx'],
-        target: 'serverless',
-    }),
+module.exports = withPreact(
+    withMDX(
+        withTypescript({
+            pageExtensions: ['tsx', 'md', 'mdx'],
+            target: 'serverless',
+        }),
+    ),
 );
