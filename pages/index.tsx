8a47d54f82207fa762afa0ae78eb5762bb727f6f
Alex Sanders <a@snd.rs>
2019-03-09 03:22:56 +0000

    more performant

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import Link from 'next/link';
 import { Component } from 'react';
-import pages from '../.scraped/urls.json';
+import pages from '../.scraped/urls';
 import Page from '../components/Page';
-
-const random = (min: number = -40, max: number = 40) =>
-    Math.floor(Math.random() * (max - min + 1)) + min;
-
-const trans = 0.6;
-const red = `rgba(255, 0, 0, ${trans})`;
-const green = `rgba(0, 255, 0, ${trans})`;
-const blue = `rgba(0, 0, 255, ${trans})`;
-const yellow = `rgba(255, 255, 0, ${trans})`;
+import ScrapedSite from '../components/ScrapedSite';
 
 export default class Home extends Component {
     render() {
         return (
             <Page align="center" direction="row">
+                <style jsx global>{`
+                    body {
+                        background-color: black;
+                    }
+                `}</style>
+
                 <h1>2MS</h1>
                 <ul>
                     <li>
@@ -38,93 +36,26 @@ export default class Home extends Component {
                         content: '·';
                     }
                 `}</style>
-                <div
-                    className="sites"
-                    dangerouslySetInnerHTML={{
-                        __html: pages[random(0, pages.length - 1)],
-                    }}
-                />
-                <style jsx global>{`
-                    @keyframes shadow {
-                        0% {
-                            text-shadow: ${random()}px 0px 0 ${red},
-                                ${random()}px 0px 0 ${green},
-                                ${random()}px 0px 0 ${blue},
-                                ${random()}px 0px 0 ${yellow};
-                        }
 
-                        10% {
-                            text-shadow: ${random()}px 0px 0 ${red},
-                                ${random()}px 0px 0 ${green},
-                                ${random()}px 0px 0 ${blue},
-                                ${random()}px 0px 0 ${yellow};
-                        }
-
-                        50% {
-                            text-shadow: ${random()}px 0px 0 ${red},
-                                ${random()}px 0px 0 ${green},
-                                ${random()}px 0px 0 ${blue},
-                                ${random()}px 0px 0 ${yellow};
-                        }
-
-                        70% {
-                            text-shadow: ${random()}px 0px 0 ${red},
-                                ${random()}px 0px 0 ${green},
-                                ${random()}px 0px 0 ${blue},
-                                ${random()}px 0px 0 ${yellow};
-                        }
-
-                        100% {
-                            text-shadow: ${random()}px 0px 0 ${red},
-                                ${random()}px 0px 0 ${green},
-                                ${random()}px 0px 0 ${blue},
-                                ${random()}px 0px 0 ${yellow};
-                        }
-                    }
-                    @keyframes resize {
-                        from {
-                            max-width: 100ch;
-                        }
-
-                        to {
-                            max-width: 0%;
-                        }
-                    }
-
-                    body {
-                        background-color: black;
-                    }
-                    .sites {
-                        position: absolute;
-                        left: 00px;
-                        top: 00px;
-                        max-width: 100ch;
+                <div>
+                    {['red', 'lime', 'blue', 'white'].map(colour => (
+                        <ScrapedSite
+                            color={colour}
+                            content={pages[2]}
+                            key={colour}
+                        />
+                    ))}
+                </div>
+                <style jsx>{`
+                    div {
+                        position: relative;
                         z-index: -1;
                         pointer-events: none;
-                        color: rgba(255, 255, 255, 0.6);
-                        animation-duration: 30s, 500s;
-                        animation-name: shadow;
-                        animation-iteration-count: infinite, 1;
-                        animation-direction: alternate, normal;
-                        animation-timing-function: ease-out, linear;
-                        animation-fill-mode: forwards;
-                        animation-delay: 0s;
-                        text-shadow: ${random()}px 0px 0 ${red},
-                            ${random()}px 0px 0 ${green},
-                            ${random()}px 0px 0 ${blue},
-                            ${random()}px 0px 0 ${yellow};
-
-                        -webkit-font-smoothing: antialiased;
                     }
-
-                    .sites * {
-                        font-weight: normal !important;
-                        font-family: 'Helvetica Neue', Helvetica, Arial,
-                            sans-serif !important;
-                        font-size: 60px !important;
-                        line-height: 32px !important;
-                        letter-spacing: -3px;
-                        word-spacing: 0.2em;
+                `}</style>
+                <style jsx global>{`
+                    body {
+                        background-color: black;
                     }
                 `}</style>
             </Page>
