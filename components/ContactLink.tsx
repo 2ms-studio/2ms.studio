7bf6b87a56528a936dbfaebd53a2703b786653e4
Alex Sanders <a@snd.rs>
2019-03-22 18:12:27 +0000

    maek contact editable by dan
    fixes #23

---
+++ components/ContactLink.tsx
@@ -0,0 +1,30 @@
+interface Props {
+	title: string
+	href: string
+}
+
+const ContactLink: React.FC<Props> = ({ href, children, title }) => (
+	<>
+		<dt>{title}</dt>
+		<dd>
+			<a href={href} rel="noopener noreferrer" target="_blank">
+				{children}
+			</a>
+		</dd>
+		<style jsx>{`
+			dt {
+				font-weight: bold;
+				text-transform: uppercase;
+				margin-top: 1rem;
+			}
+
+			a,
+			a:visited,
+			a:active {
+				color: #00f;
+			}
+		`}</style>
+	</>
+)
+
+export default ContactLink
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,13 @@
+import ContactLink from '../components/ContactLink'
+import ContactItems from '../dan-edits-these/contact.mdx'
+
 const Contact: React.FC = () => (
 	<>
 		<dl>
-			<dt>email</dt>
-			<dd>
-				<a href="mailto:dan@2MS.studio">dan@2MS.studio</a>
-			</dd>
-			<dt>website by</dt>
-			<dd>
-				<a href="https://sndrs.dev">sndrs.dev</a>
-			</dd>
+			<ContactItems />
+			<ContactLink title="Website by" href="https://sndrs.dev">
+				sndrs.dev
+			</ContactLink>
 		</dl>
 		<style jsx>{`
 			dl {
@@ -17,27 +16,12 @@ const Contact: React.FC = () => (
 				left: 50%;
 				transform: translate(-50%, -50%) rotate(-1deg);
 				background-color: white;
-				padding: 2rem;
+				padding: 1rem 2rem 2rem;
 				z-index: 15;
 				font-size: 14px;
 				box-shadow: 15px 15px 0 rgba(255, 255, 255, 0.2);
 				color: black;
 				background-blend-mode: multiply;
-			}
-
-			dt {
-				font-weight: bold;
-				text-transform: uppercase;
-			}
-
-			dd + dt {
-				margin-top: 1rem;
-			}
-
-			a,
-			a:visited,
-			a:active {
-				color: #00f;
 			}
 		`}</style>
 	</>
