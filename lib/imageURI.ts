845075fdf9eb6c27698608977f2a0edcc9a1c146
Alex Sanders <a@snd.rs>
2019-10-02 12:11:47 +0100

    update deps

--- lib/imageURI.ts
+++ lib/imageURI.ts
@@ -6,6 +6,4 @@ const uploads: {
 } = require('../image_uploads.json') // eslint-disable-line @typescript-eslint/no-var-requires
 
 export default ({ width, src }: { width: number; src: string }) =>
-	`https://res.cloudinary.com/twoms/image/upload/f_auto,q_60,c_scale,w_${width}/${
-		uploads[src].id
-	}`
+	`https://res.cloudinary.com/twoms/image/upload/f_auto,q_60,c_scale,w_${width}/${uploads[src].id}`
