05f086d1f5ea5766fcacb99578b5624197532c84
Alex Sanders <a@snd.rs>
2020-02-20 16:39:42 +0000

    dont need this

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,9 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
-
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head />
 				<body>
 					<Main />
