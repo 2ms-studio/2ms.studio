b02bb57d3e40c0c4d1186c5d9f0d115e6763fe70
sndrs <alex@sndrs.dev>
2022-08-21 14:20:46 +0000

    Deploying to gh-pages from @ 2ms-studio/2ms.studio@2c3858f68f59f1f926426c1bb0f69a0484f53079 🚀

---
+++ _next/static/chunks/pages/index-b727d10900cf5a6b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{9369:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return c(2526)}])},2526:function(a,b,c){"use strict";c.r(b);var d=c(2937),e=function(){return(0,d.tZ)("div",{})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=9369)}),_N_E=a.O()}])
